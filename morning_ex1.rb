ba8bde392d241f603a54802ea014fa62572aca34
Sam Lim <limsammy1@gmail.com>
2017-03-15 10:42:00 -0600

    Final version, converted b array to fixnum

--- morning_ex1.rb
+++ morning_ex1.rb
@@ -1,11 +1,9 @@
-require 'pry'
 one_to_one_hundred_squared_a = *(1..100).map {|num| num ** 2}
 
 a = one_to_one_hundred_squared_a.inject(:+)
 
 
 one_to_one_hundred_b = *(1..100).inject(:+)
-b = one_to_one_hundred_b.map {|num| num ** 2}
-binding.pry
+b = one_to_one_hundred_b.map {|num| num ** 2}[0]
 
 puts b - a
