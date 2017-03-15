2dc07e3a1516afa2b586efcc50d0726291552d19
Sam Lim <limsammy1@gmail.com>
2017-03-15 10:39:39 -0600

    added math for variables a and b, TODO: convert b from array to fixnum

---
+++ morning_ex1.rb
@@ -0,0 +1,11 @@
+require 'pry'
+one_to_one_hundred_squared_a = *(1..100).map {|num| num ** 2}
+
+a = one_to_one_hundred_squared_a.inject(:+)
+
+
+one_to_one_hundred_b = *(1..100).inject(:+)
+b = one_to_one_hundred_b.map {|num| num ** 2}
+binding.pry
+
+puts b - a
