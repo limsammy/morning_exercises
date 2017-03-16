72885afcc18be80ac2e5add62e7f6cd995f7e291
Sam Lim <limsammy1@gmail.com>
2017-03-16 09:27:49 -0600

    Wrote atIndex(n) method to calculate nth term of the fib sequence, wrote range(n) method that uses atIndex to write out all fib numbers up to n

---
+++ fibonacci.rb
@@ -0,0 +1,21 @@
+def atIndex(n)
+	a = 0
+	b = 1
+
+	n.times do
+		temp = a
+		a = b
+		b = temp + b
+	end
+
+	return a
+end
+
+def range(n)
+	n.times do |i|
+		final = atIndex(i)
+		puts final
+	end
+end
+
+range(25)
