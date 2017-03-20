472b7fbe535fa60441f82685829d7bfe0384db72
Sam Lim <limsammy1@gmail.com>
2017-03-20 09:27:07 -0600

    created leap years array to store leap years

--- leap_year.rb
+++ leap_year.rb
@@ -5,6 +5,7 @@ class LeapYear
   def predict(leap_years)
     start_year = 2017
     leap_count = 0
+    years = []
 
     while leap_count < leap_years do
       leap_years.times do |year|
@@ -12,16 +13,16 @@ class LeapYear
 
         if year % 4 == 0
           leap_count += 1
-          puts "#{year} is a leap year!"
+          years << year
+          # puts "#{year} is a leap year!"
         elsif year % 400 == 0
           leap_count += 1
-          puts "#{year} is a leap year!"
-        else
-          puts "#{year} is not a leap year!"
+          years << year
+          # puts "#{year} is a leap year!"
         end
-
       end
     end
+    puts years.size
   end
 
 end
