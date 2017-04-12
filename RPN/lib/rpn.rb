a854707084b143f6fe969e58586499d2bd264a07
Sam Lim <limsammy1@gmail.com>
2017-04-12 09:32:19 -0600

    RPN calculator meets base expectations for two expressions

---
+++ RPN/lib/rpn.rb
@@ -0,0 +1,27 @@
+require 'pry'
+
+class RPN
+  attr_reader :first_val, :second_val
+
+  def initialize
+    @first_val = nil
+    @second_val = nil
+    @operator = nil
+  end
+
+  def calc(expression)
+    expression_collection = expression.split(' ')
+    @first_val = expression_collection.first.to_i
+    @second_val = expression_collection[1].to_i
+    @operator = expression_collection.last
+    check_operator(@operator)
+  end
+
+  def check_operator(operator)
+    if @operator == '+'
+      first_val + second_val
+    elsif @operator == '-'
+      first_val - second_val
+    end
+  end
+end
---
+++ RPN/test/rpn_test.rb
@@ -0,0 +1,36 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require_relative '../lib/rpn'
+
+class RPNTest < Minitest::Test
+  def setup
+    @rpn = RPN.new
+  end
+
+  def test_rpn_object_exists
+    assert_instance_of RPN, @rpn
+  end
+
+  def test_respond_to_calc_func
+    assert_respond_to(@rpn, :calc)
+  end
+
+  def test_calc_func_assigns_expression_ints_to_instance_vars
+    @rpn.calc('5 6')
+    assert_equal 5, @rpn.first_val
+    assert_equal 6, @rpn.second_val
+  end
+
+  def test_calc_func_parses_string_nums_into_fixnums
+    @rpn.calc('1 2')
+    assert_equal 1, @rpn.first_val
+  end
+
+  def test_calc_func_succesfully_calculates_sum
+    assert_equal 3, @rpn.calc('1 2 +')
+  end
+
+  def test_calc_func_can_find_difference
+    assert_equal 2, @rpn.calc('5 3 -')
+  end
+end
